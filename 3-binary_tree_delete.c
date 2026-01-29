3e4856f2329bfd7b2d116cebff083b660d85553b
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-29 13:07:52 +0000

    1

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_tree.h"
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
